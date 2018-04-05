f4adb5380252b85782b09c3a236187c9c94cda3c
isabelledl <league@wts-imac-11.attlocal.net>
2018-04-04 18:26:26 -0700

    4/4, we did Secret mEssage Box and Remarkable.

---
+++ src/Remarkable.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable {
+	public static void main(String[] args) {
+		for (int i=0;i<10;i++) {
+		String answer = JOptionPane.showInputDialog(null, "What is name?");
+		if (answer.equals("Mike")) {
+JOptionPane.showMessageDialog(null,"Mike like fish");
+		}
+
+		if (answer.equals("Grace")) {
+JOptionPane.showMessageDialog(null,"This guy enjoy Salmoon crazy.");
+		}
+		if (answer.equals("William")) {
+JOptionPane.showMessageDialog(null,"I don't know this guy much.");
+		}
+
+	}
+	}
+}
+
